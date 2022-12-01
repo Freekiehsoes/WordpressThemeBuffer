7bf32435ee4e68a7998ecf7fcc5b149276b56fe5
freekattema <freekattema@live.nl>
2022-12-01 14:11:22 +0100

    Added theme buffer

---
+++ src/Buffer.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace Freekattema\WordpressThemeBuffer;
+
+final class Buffer {
+	/** @var Buffer|null  */
+    private static $instance = null;
+
+    /** @var array  */
+	private static $replace = [];
+    /** @var array  */
+	private static $in_header = [];
+    /** @var array  */
+	private static $in_footer = [];
+
+    private function __construct() {
+        ob_start(function($content) {
+            $this->before_replace();
+
+            foreach(self::$replace as $rule) {
+                $content = preg_replace($rule[0], $rule[1], $content);
+            }
+
+            return $content;
+        });
+    }
+
+    private function before_replace(): void
+    {
+        $headerRules = implode(' ', self::$in_header);
+        $footerRules = implode(' ', self::$in_footer);
+
+        self::$replace[] = [
+            "/<\/head>/",
+            $headerRules . "\n</head>"
+        ];
+
+        self::$replace[] = [
+            "/<\/body>/",
+            $footerRules . "\n</body>"
+        ];
+    }
+
+    public static function init(): void
+    {
+        if (self::$instance == null) {
+            self::$instance = new Buffer();
+        }
+    }
+
+    public static function replace(string $search, string $replace): void
+    {
+        if(!self::is_valid_regex($search)) {
+            $search = preg_quote($search);
+        }
+
+        self::$replace[] = [$search, $replace];
+    }
+
+    public static function add_to_header(string $content): void
+    {
+        self::$in_header[] = $content;
+    }
+
+    public static function add_to_footer(string $content): void
+    {
+        self::$in_footer[] = $content;
+    }
+
+    public static function add_css(string $path): void
+    {
+        self::add_to_header("<link rel='stylesheet' href='$path'>");
+    }
+
+    public static function add_js(string $path, $defer = true): void
+    {
+        $defer = $defer ? 'defer' : '';
+        self::add_to_footer("<script src='$path' $defer></script>");
+    }
+
+    private static function is_valid_regex(string $regex): bool
+    {
+        return @preg_match($regex, '') !== false;
+    }
+}
---
+++ src/autoinit.php
@@ -0,0 +1,3 @@
+<?php
+
+\Freekattema\WordpressThemeBuffer\Buffer::init();
